1fe9a35d374592271ccdaa96afccf3d95d49c7d9
matheusdalia <matheusdalia8@gmail.com>
2022-03-31 20:51:19 -0300

    Deploy2

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import { RedButton, BlueButton } from './Button';
 import { Input } from './Input';
 import { Toast } from './Toast';
-import { NavBar } from './NavBar';
+import NavBar from './NavBar';
 
 export { RedButton, BlueButton, Input, Toast, NavBar };
