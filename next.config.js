bfe8a2e0f7f39343dafb7e0892ba4b85be749898
matheusdalia <matheusdalia8@gmail.com>
2022-04-01 02:20:27 -0300

    hope

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 const withPWA = require('next-pwa');
 const isProd = process.env.NODE_ENV === 'production';
 
+module.exports = {
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+}
+
 module.exports = withPWA({
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-  reactStrictMode: true,
+  reactStrictMode: false,
   pwa: {
     dest: 'public',
     disable: !isProd
